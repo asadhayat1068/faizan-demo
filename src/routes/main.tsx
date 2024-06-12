9a602b37633ff31c706ab3fa866b2a53dc903c02
Asad Hayat <asadhayat.1068@gmail.com>
2024-06-13 02:36:05 +0500

    Typo fixed

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Redeem from "../pages/Redeem";
 import NotFoundPage from "../pages/NotFoundPage";
-import About from '../pages/About';
-import Contact from '../pages/Contact';
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 
 const MainRouter: React.FC = () => {
   return (
