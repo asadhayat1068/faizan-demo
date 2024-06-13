38bef28a307c2edef142b9df2ab2186a79ebc06c
Asad Hayat <asadhayat.1068@gmail.com>
2024-06-13 05:02:35 +0500

    Setup Wagmi,RainbowKit with Connect Wallet Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { Outlet } from "react-router-dom";
-
 import Header from "./components/Header/Header";
 import "./App.css";
 import "./styles/tailwind.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { WagmiProvider } from "wagmi";
+import { defaultConfig, queryClient } from "./wallet";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 function App() {
   return (
     <div className="page-wrapper">
-      <div id="header">
-        <Header />
-      </div>
-      <div id="main">
-        <Outlet />
-      </div>
+      <WagmiProvider config={defaultConfig}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <div id="header">
+              <Header />
+            </div>
+            <div id="main">
+              <Outlet />
+            </div>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </div>
   );
 }
