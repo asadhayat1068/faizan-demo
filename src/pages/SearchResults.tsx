a5487d0718e9c34b8febceac7cd81166effb74f5
Asad Hayat <asadhayat.1068@gmail.com>
2024-08-14 02:00:10 +0500

    Bug fixes after merge

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useLocation, Link, useNavigate } from 'react-router-dom';
-import { useAPI } from '../apiContext';
-import Card from '../components/Card';
-import Spinner from '../components/Spinner';
-import Fuse from 'fuse.js';
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useAPI } from "../apiContext";
+import Card from "../components/Card";
+import Spinner from "../components/Spinner";
+import Fuse from "fuse.js";
 
 // Define types for attributes and search results
 type CustomAttribute = {
@@ -18,7 +18,7 @@ type Product = {
   price_usd: number;
   custom_attributes: CustomAttribute[];
   media_gallery_entries: { file: string }[];
-  conversionRate:number;
+  conversionRate: number;
 };
 
 type SearchResultsResponse = {
@@ -33,7 +33,7 @@ const SearchResults: React.FC = () => {
   const { searchResults, performSearch, categories } = useAPI();
   const query = useQuery();
   const navigate = useNavigate();
-  const searchQuery = query.get('query') || '';
+  const searchQuery = query.get("query") || "";
   const hasSearched = useRef(false);
   const [loading, setLoading] = useState(false);
   const [suggestions, setSuggestions] = useState<string[]>([]);
@@ -59,17 +59,22 @@ const SearchResults: React.FC = () => {
 
   const suggestTerms = (query: string) => {
     const fuse = new Fuse(categories, {
-      keys: ['name'],
+      keys: ["name"],
       threshold: 0.3,
     });
     const result = fuse.search(query);
-    const suggestedTerms = result.map(item => item.item.name);
+    const suggestedTerms = result.map((item) => item.item.name);
     setSuggestions(suggestedTerms);
   };
 
-  const extractAttribute = (attributes: CustomAttribute[], attributeCode: string): string | number => {
-    const attribute = attributes.find(attr => attr.attribute_code === attributeCode);
-    return attribute ? attribute.value : 'N/A';
+  const extractAttribute = (
+    attributes: CustomAttribute[],
+    attributeCode: string
+  ): string | number => {
+    const attribute = attributes.find(
+      (attr) => attr.attribute_code === attributeCode
+    );
+    return attribute ? attribute.value : "N/A";
   };
 
   const handleSearch = (term: string) => {
@@ -78,7 +83,7 @@ const SearchResults: React.FC = () => {
     setLoading(true);
     performSearch(term, 16, 1).then(() => setLoading(false)); // Trigger search and set loading
   };
-console.log(searchResults);
+  console.log(searchResults);
   return (
     <div className="container mx-auto p-6">
       {loading && <Spinner />}
@@ -89,9 +94,10 @@ console.log(searchResults);
             {searchResults.map((product) => {
               const imgSrc = product.media_gallery_entries?.[0]?.file
                 ? `https://cryptrovia.com/pub/media/catalog/product${product.media_gallery_entries[0].file}`
-                : 'https://via.placeholder.com/400';
-                let usdValue = extractAttribute(product.custom_attributes, 'usd');
-                usdValue = typeof usdValue === 'string' ? parseFloat(usdValue) : usdValue;
+                : "https://via.placeholder.com/400";
+              let usdValue = extractAttribute(product.custom_attributes, "usd");
+              usdValue =
+                typeof usdValue === "string" ? parseFloat(usdValue) : usdValue;
               return (
                 <Card
                   id={product.id}
@@ -100,6 +106,7 @@ console.log(searchResults);
                   price_eth={product.price_eth}
                   key={product.id}
                   price_usd={product.price_usd}
+                  sku={product.sku}
                 />
               );
             })}
--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,5 +1,4 @@
-import axios, { AxiosError } from 'axios';
-
+import axios, { AxiosError } from "axios";
 
 // Define types for responses
 interface TokenResponse {
@@ -32,32 +31,29 @@ interface HomeProductsResponse {
 interface ProductDetails {
   id: number;
   name: string;
-  sku:string;
+  sku: string;
   description: string;
   price_eth: number;
   price_usd: number;
   imageUrl: string;
   custom_attributes: CustomAttribute[];
-  conversionRate:number;
+  conversionRate: number;
   media_gallery_entries: Array<{ file: string }>;
 }
 
-
-const tokenURL = 'https://cryptrovia.com/api/token.php';
-const categoryURL = 'https://cryptrovia.com/api/category.php';
-const categoryproducts = 'https://cryptrovia.com/api/category_products.php';
-const homeItems = 'https://cryptrovia.com/api/homeitems.php';
-const productDetailsURL = 'https://cryptrovia.com/api/getproductdetails.php';
-const redeemProductURL = 'https://cryptrovia.com/api/redeem_product_api.php';
-const searchUrl = 'https://cryptrovia.com/api/itmsearch.php';
-
-
+const tokenURL = "https://cryptrovia.com/api/token.php";
+const categoryURL = "https://cryptrovia.com/api/category.php";
+const categoryproducts = "https://cryptrovia.com/api/category_products.php";
+const homeItems = "https://cryptrovia.com/api/homeitems.php";
+const productDetailsURL = "https://cryptrovia.com/api/getproductdetails.php";
+const redeemProductURL = "https://cryptrovia.com/api/redeem_product_api.php";
+const searchUrl = "https://cryptrovia.com/api/itmsearch.php";
 
 const fetchToken = async (): Promise<string> => {
   try {
-    const cachedToken = localStorage.getItem('token');
-    const cachedExpiration = localStorage.getItem('tokenExpiration');
-   
+    const cachedToken = localStorage.getItem("token");
+    const cachedExpiration = localStorage.getItem("tokenExpiration");
+
     // Check if the cached token is still valid
     if (cachedToken && cachedExpiration) {
       const currentTime = new Date().getTime();
@@ -72,8 +68,10 @@ const fetchToken = async (): Promise<string> => {
     console.log(response);
     const token: string = response.data;
 
-    if (!token || typeof token !== 'string') {
-      throw new Error('Token received from server is null, undefined, or not a string');
+    if (!token || typeof token !== "string") {
+      throw new Error(
+        "Token received from server is null, undefined, or not a string"
+      );
     }
 
     // Calculate token expiration time
@@ -81,81 +79,91 @@ const fetchToken = async (): Promise<string> => {
     const expirationTime = new Date().getTime() + TOKEN_EXPIRATION_TIME * 1000;
 
     // Store new token and expiration time in local storage
-    localStorage.setItem('token', token);
-    localStorage.setItem('tokenExpiration', expirationTime.toString());
+    localStorage.setItem("token", token);
+    localStorage.setItem("tokenExpiration", expirationTime.toString());
 
     //console.log('Fetched and stored new token:', token);
     return token;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
-      console.error('Error fetching token:', error.response ? error.response.data : error.message);
+      console.error(
+        "Error fetching token:",
+        error.response ? error.response.data : error.message
+      );
     } else {
-      console.error('Error fetching token:', error);
+      console.error("Error fetching token:", error);
     }
     throw error;
   }
 };
 
-
-
 const fetchCategories = async (token: string): Promise<any> => {
   try {
     const headers = {
       Authorization: `${token}`,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     };
     const response = await axios.post(categoryURL, {}, { headers });
     return response.data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       if (error.response && error.response.status === 401) {
-        throw new Error('TOKEN_EXPIRED');
+        throw new Error("TOKEN_EXPIRED");
       }
       throw error;
     } else {
-      console.error('Error fetching categories:', error);
+      console.error("Error fetching categories:", error);
       throw error;
     }
   }
 };
 
-const fetchCategoryProducts = async (token: string, categoryId: number, pageSize = 10, currentPage = 1): Promise<any> => {
+const fetchCategoryProducts = async (
+  token: string,
+  categoryId: number,
+  pageSize = 10,
+  currentPage = 1
+): Promise<any> => {
   try {
     const headers = {
       Authorization: `${token}`,
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     };
 
     const data = {
       categoryId,
       pageSize,
-      currentPage
+      currentPage,
     };
 
     const response = await axios.post(categoryproducts, data, { headers });
-   console.log(response.data);
+    console.log(response.data);
     return response.data;
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       if (error.response && error.response.status === 401) {
-        throw new Error('TOKEN_EXPIRED');
+        throw new Error("TOKEN_EXPIRED");
       }
       throw error;
     } else {
-      console.error('Error fetching category products:', error);
+      console.error("Error fetching category products:", error);
       throw error;
     }
   }
 };
 
 // Function to fetch home products
-const fetchHomeProducts = async (token: string, pageSize: number, currentPage: number): Promise<Category[]> => {
+const fetchHomeProducts = async (
+  token: string,
+  pageSize: number,
+  currentPage: number
+): Promise<Category[]> => {
   const categoryIds = [3577, 4189, 4188, 3578, 3581, 3582, 3583, 3572, 3579];
 
   try {
     const headers = {
       Authorization: `${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     const data = {
@@ -168,39 +176,50 @@ const fetchHomeProducts = async (token: string, pageSize: number, currentPage: n
 
     // Ensure response data is in expected format
     if (!response.data || !Array.isArray(response.data.categories)) {
-      throw new Error('Invalid data structure');
+      throw new Error("Invalid data structure");
     }
 
-    const structuredData: Category[] = response.data.categories.map((category: any) => ({
-      categoryId: category.categoryId,
-      categoryName: category.categoryName,
-      conversionRate:category.conversion_rate,
-      products: category.items.map((item: any) => ({
-        id: item.id,
-        name: item.name,
-        price_eth: item.price_eth,
-        price_usd: item.price_usd,
-        description: item.custom_attributes.find((attr: any) => attr.attribute_code === 'description')?.value || '',
-        usd: item.custom_attributes.find((attr: any) => attr.attribute_code === 'usd')?.value || '',
-        price: item.price,
-        imageUrl: item.media_gallery_entries?.[0]?.file
-          ? `https://cryptrovia.com/pub/media/catalog/product${item.media_gallery_entries[0].file}`
-          : 'https://via.placeholder.com/400',
-      })),
-    }));
+    const structuredData: Category[] = response.data.categories.map(
+      (category: any) => ({
+        categoryId: category.categoryId,
+        categoryName: category.categoryName,
+        conversionRate: category.conversion_rate,
+        products: category.items.map((item: any) => ({
+          id: item.id,
+          name: item.name,
+          price_eth: item.price_eth,
+          price_usd: item.price_usd,
+          description:
+            item.custom_attributes.find(
+              (attr: any) => attr.attribute_code === "description"
+            )?.value || "",
+          usd:
+            item.custom_attributes.find(
+              (attr: any) => attr.attribute_code === "usd"
+            )?.value || "",
+          price: item.price,
+          imageUrl: item.media_gallery_entries?.[0]?.file
+            ? `https://cryptrovia.com/pub/media/catalog/product${item.media_gallery_entries[0].file}`
+            : "https://via.placeholder.com/400",
+        })),
+      })
+    );
 
     return structuredData;
   } catch (error) {
-    console.error('Error fetching home products:', error);
+    console.error("Error fetching home products:", error);
     throw error;
   }
 };
 
-const fetchProductDetails = async (token: string, productId: number): Promise<ProductDetails> => {
+const fetchProductDetails = async (
+  token: string,
+  productId: number
+): Promise<ProductDetails> => {
   try {
     const headers = {
       Authorization: `${token}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
 
     const data = {
@@ -209,29 +228,32 @@ const fetchProductDetails = async (token: string, productId: number): Promise<Pr
 
     const response = await axios.post(productDetailsURL, data, { headers });
     const item = response.data.items[0];
-    
+
     return {
       id: item.id,
       name: item.name,
-      sku:item.sku,
-      description: item.custom_attributes.find((attr: CustomAttribute) => attr.attribute_code === 'description')?.value || '',
+      sku: item.sku,
+      description:
+        item.custom_attributes.find(
+          (attr: CustomAttribute) => attr.attribute_code === "description"
+        )?.value || "",
       price_eth: item.price_eth,
       price_usd: item.price_usd,
-      conversionRate:response.data.conversion_rate,
+      conversionRate: response.data.conversion_rate,
       custom_attributes: item.custom_attributes,
       media_gallery_entries: item.media_gallery_entries,
       imageUrl: item.media_gallery_entries?.[0]?.file
         ? `https://cryptrovia.com/pub/media/catalog/product${item.media_gallery_entries[0].file}`
-        : 'https://via.placeholder.com/400',
+        : "https://via.placeholder.com/400",
     };
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       if (error.response && error.response.status === 401) {
-        throw new Error('TOKEN_EXPIRED');
+        throw new Error("TOKEN_EXPIRED");
       }
       throw error;
     } else {
-      console.error('Error fetching product details:', error);
+      console.error("Error fetching product details:", error);
       throw error;
     }
   }
@@ -241,18 +263,23 @@ const sendWalletId = async (walletId: string): Promise<any> => {
   try {
     const response = await axios.post(redeemProductURL, null, {
       headers: {
-        'Authorization': `${walletId}`,
-        'Content-Type': 'application/json'
-      }
+        Authorization: `${walletId}`,
+        "Content-Type": "application/json",
+      },
     });
     return response.data;
   } catch (error: unknown) {
-    console.error('Error sending wallet ID:', error);
+    console.error("Error sending wallet ID:", error);
     throw error;
   }
 };
 
-const searchProducts = async (token: string, searchQuery: string, pageSize = 10, currentPage = 1): Promise<any> => {
+const searchProducts = async (
+  token: string,
+  searchQuery: string,
+  pageSize = 10,
+  currentPage = 1
+): Promise<any> => {
   try {
     const response = await axios.post(
       searchUrl,
@@ -263,17 +290,57 @@ const searchProducts = async (token: string, searchQuery: string, pageSize = 10,
       },
       {
         headers: {
-          'Authorization': `${token}`,
-          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+          "Content-Type": "application/json",
         },
       }
     );
     console.log(response.data);
     return response.data;
   } catch (error: unknown) {
-    console.error('Error searching products:', error);
+    console.error("Error searching products:", error);
     throw error;
   }
 };
 
-export { searchProducts, fetchToken, fetchCategories, fetchCategoryProducts, fetchHomeProducts, fetchProductDetails, sendWalletId };
+export const getMintPriceAndSignature = async (
+  token: string,
+  walletAddress: string,
+  paymentToken: string,
+  productSKU: string
+) => {
+  try {
+    const headers = {
+      Authorization: `${token}`,
+      "Content-Type": "application/json",
+    };
+
+    const data = {
+      usrAdd: walletAddress,
+      sku: productSKU,
+      paymentToken: paymentToken,
+    };
+
+    const response = await axios.post(
+      "https://cryptrovia.com/itemsign.php",
+      data,
+      { headers }
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 401) {
+      throw new Error("TOKEN_EXPIRED");
+    }
+    throw error;
+  }
+};
+
+export {
+  searchProducts,
+  fetchToken,
+  fetchCategories,
+  fetchCategoryProducts,
+  fetchHomeProducts,
+  fetchProductDetails,
+  sendWalletId,
+};
